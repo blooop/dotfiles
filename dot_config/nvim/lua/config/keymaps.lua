9aa9cd65c8e27d4fe302228d95ecd8944f25c8bb
Austin Gregg-Smith <blooop@gmail.com>
2025-12-07 16:18:38 +0000

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- VSCode-style comment toggle with Ctrl+/
+-- Note: In most terminals, Ctrl+/ is sent as Ctrl+_
+vim.keymap.set("n", "<C-_>", "gcc", { remap = true, desc = "Toggle comment" })
+vim.keymap.set("v", "<C-_>", "gc", { remap = true, desc = "Toggle comment" })
+vim.keymap.set("i", "<C-_>", "<Esc>gcca", { remap = true, desc = "Toggle comment" })
