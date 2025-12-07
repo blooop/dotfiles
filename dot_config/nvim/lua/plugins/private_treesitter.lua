03c1da0c2743ed3fe440556c7bbdcaeaa910023f
Austin Gregg-Smith <blooop@gmail.com>
2025-12-07 18:01:01 +0000

    Add .config/nvim/lua/plugins/treesitter.lua

---
+++ dot_config/nvim/lua/plugins/private_treesitter.lua
@@ -0,0 +1,48 @@
+-- Treesitter configuration
+-- Configure which language parsers to install automatically
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      -- Only install these languages on first launch
+      -- Add more as needed, or use "all" to install everything (slower)
+      ensure_installed = {
+        -- Essential
+        "bash",
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+
+        -- Programming languages (add only what you use)
+        "python",
+        "c",
+        "cpp",
+        -- "rust",
+        -- "go",
+        -- "java",
+        -- "javascript",
+        -- "typescript",
+
+        -- Config files
+        "json",
+        "yaml",
+        "toml",
+        -- "dockerfile",
+        -- "cmake",
+
+        -- Other
+        "regex",
+        "query",  -- for treesitter query files
+      },
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      auto_install = true,
+    },
+  },
+}
